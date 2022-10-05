c621e41188cb62e2e1e66eb47c85d08b1cf0a37b
Josephine <sosodjibrildiop@gmail.com>
2022-10-05 09:25:29 +0000

    page-accueil

---
+++ page_accueil.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>page_d'accueil</title>
+    <link rel="stylesheet" href="mon_style.css">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+
+
+
+    <div class="container-fluid">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                <nav class="navbar navbar-expand-lg navbar-light bg-white">
+                    <img src="image/logo2.png" alt="monlogo" class="logo">
+                    <a class="navbar-brand" href="#">Ecole de la Reussite</a>
+                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav link">
+                 <li class="nav-item active">
+                    <a class="nav-link" href="#">ACCUEIL<span class="sr-only">(current)</span></a>
+                 </li>
+                 <li class="nav-item">
+                    <a class="nav-link" href="#">PRESENTATIONS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">PEDAGOGIE</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">SERVICES</a>
+                </li>
+                <li class="nav-item">
+                     <a class="nav-link mr-5" href="#">CONTACT</a>
+                </li>
+                 <div class="inscrire mr-5">  
+                <li class="nav-item dropdown ">
+                 <a class="nav-link dropdown-toggle  "  href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                S'INSCRIRE
+                </a>
+                <div class="dropdown-menu " aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">Eleve</a>
+                <a class="dropdown-item" href="#">Employer</a>
+              
+                </div>
+                </li>
+                </div>
+            <div class="connecte">    
+                  <li class="nav-item">
+                    <a class="nav-link " href="#">SE CONNECTER</a>
+                  </li>
+            </div>  
+      
+            </ul>
+            </div>
+            </nav>
+         </div>
+     </div>
+  </div>
+
+<!-- SEction 2 -->
+<div class="row sec2">
+    <div class="col-12">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" >
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="image/img1.jpg" alt="First slide">
+            </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="image/img2.jpg" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="image/img3.jpg" alt="Third slide">
+          </div>
+        </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+          </a>
+        </div>
+    </div>
+</div>
+<!--ENd SEction 2 -->
+<!-- Section 3 -->
+
+<h1 class="titre text-center mt-3">Nos différentes Cycles </h1>
+<div class="row sec3">
+    
+    <div class="col-1"> </div>
+   
+    <div class="col-5"> 
+    <div class="card mt-5" >
+            <div class="card-body">
+            <h5 class="card-title text-dark"><strong>Primaire</strong></h5>
+            <h6 class="card-subtitle mb-2 text-muted"></h6>
+            <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe ullam, cum minus ducimus placeat repudiandae reprehenderit incidunt voluptatum eius officia recusandae id deserunt a beatae doloribus nihil dolores? Similique, magni..</p>
+            <a href="#" class="card-link"><button class="btn text-dark">Savoir plus</button></a>
+           
+            </div>
+            </div>
+    </div>
+    <div class="col-5"> 
+    <div class="card mt-5" >
+            <div class="card-body">
+            <h5 class="card-title text-dark"><strong>Secondaire</strong></h5>
+            <h6 class="card-subtitle mb-2 text-muted"></h6>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas cumque laboriosam sapiente beatae enim odio praesentium nam temporibus eius, neque autem reprehenderit dolore consequuntur, officia ullam possimus quos deleniti provident.</p>
+            <a href="#" class="card-link " ><button class="btn text-dark">Savoir plus</button></a>
+            </div>
+            </div>
+    </div>
+    <div class="col-1"> </div>
+
+   
+</div>
+<!-- End Section3 -->
+<!-- Section4 -->
+<h1 class="titre text-center mt-3">Présentation L'école</h1>
+<div class="row sec4">
+
+    <!-- <div class="col-1"> </div> -->
+    <div class="col-12"> 
+    <div class="card mt-5" >
+        <div class="card-body">
+            <h5 class="card-title"><strong>L'école de la Réussite</strong></h5>
+            <h6 class="card-subtitle mb-2 text-muted"></h6>
+            <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing eli
+            Lorem, ipsum dolor sit amet consectetur adipisicing eli
+            Lorem, ipsum dolor sit amet consectetur adipisicing eli
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe ullam, cum minus ducimus placeat repudiandae reprehenderit incidunt voluptatum eius officia recusandae id deserunt a beatae doloribus nihil dolores? Similique, magni..</p>
+          
+        </div>
+    </div>
+    </div>
+   
+    
+ 
+
+   
+</div>
+<!-- End Section4 -->
+<!-- Start Section 5 -->
+<div class="row mt-5">
+    <div class="col-12">
+        <h1 class="titre text-center">Les images de L'école</h1>
+    </div>
+  
+    <div class="col-4 w-25">
+    <img src="image/img3.jpg " alt="img3">
+    </div>
+    <div class="col-6 ">
+    <div class="card-body">
+            <h5 class="card-title text-dark"><strong>Secondaire</strong></h5>
+            <h6 class="card-subtitle mb-2 text-muted"></h6>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas cumque laboriosam sapiente beatae enim odio praesentium nam temporibus eius, neque autem reprehenderit dolore consequuntur, officia ullam possimus quos deleniti provident.</p>
+            <a href="#" class="card-link " ><button class="btn text-dark">Savoir plus</button></a>
+            </div>
+            </div>
+    </div>
+
+    <div class="col-9 ">
+    <div class="card-body">
+            <h5 class="card-title text-dark"><strong>Primaire</strong></h5>
+            <h6 class="card-subtitle mb-2 text-muted"></h6>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+            Lorem ipsum dolor sit amet consectetur adipisicing elit
+            <var> Lorem ipsum dolor sit amet consectetur adipisicing elit</var>. Voluptas cumque laboriosam sapiente beatae enim odio praesentium nam temporibus eius, neque autem reprehenderit dolore consequuntur, officia ullam possimus quos deleniti provident.</p>
+            <a href="#" class="card-link " ><button class="btn text-dark">Savoir plus</button></a>
+            </div>
+            </div>
+    </div>
+   
+   
+
+</div>
+<!-- End Section 5 -->
+<!-- Footer -->
+<div class="col-12">
+
+</div>
+
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> 
+</body>
+</html>
---
+++ tempCodeRunnerFile.php
@@ -0,0 +1,70 @@
+<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>page_d'accueil</title>
+    <link rel="stylesheet" href="accueil.css">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                <nav class="navbar navbar-expand-lg navbar-light bg-white">
+                    <img src="image/logo2.png" alt="monlogo" class="logo">
+                    <a class="navbar-brand" href="#">Ecole de la Reussite</a>
+                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+      </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">ACCUEIL<span class="sr-only">(current)</span></a>
+          </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">PRESENTATIONS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">PEDAGOGIE</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">SERVICES</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mr-5" href="#">CONTACT</a>
+            </li>
+            <div class="inscrire mr-5">  
+            <li class="nav-item dropdown ">
+              <a class="nav-link dropdown-toggle  "  href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                S'INSCRIRE
+              </a>
+              <div class="dropdown-menu " aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">Eleve</a>
+                <a class="dropdown-item" href="#">Employer</a>
+              
+              </div>
+            </li>
+            </div>
+            <div class="connecte">    
+                  <li class="nav-item">
+                    <a class="nav-link " href="#">SE CONNECTER</a>
+                  </li>
+            </div>  
+      
+    </ul>
+  </div>
+</nav>
+                </div>
+            </div>
+        </div>
+
+
+
+
+    </div>
+</body>
+</html>
